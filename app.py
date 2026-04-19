546585e38b2b434c9e31fbb258337ad153c7e201
Harsha G <harsha150904@gmail.com>
2026-04-19 22:22:57 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/submit', methods=['POST'])
+def submit():
+    name = request.form.get('name')
+    return f"Hello {name}"
+
+if __name__ == '__main__':
+    app.run(debug=True)
